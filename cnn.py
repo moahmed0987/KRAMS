64c13d869c5a30800a31601da3744c1d3096bc17
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2024-03-13 16:41:10 +0000

    extract convnet to its own file

---
+++ cnn.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * (64//4) * (64//4), 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * (64//4) * (64//4))
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
--- train_cnn.py
+++ train_cnn.py
@@ -9,6 +9,7 @@ from matplotlib import pyplot as plt
 
 import RecordingDataset
 import training_data_processor as tdp
+from cnn import ConvNet
 
 WINDOW_SIZE = 1023
 HOP_SIZE = 225
@@ -33,23 +34,6 @@ test_indices = list(set(range(len(df))) - set(train_indices))
 
 train_df = df.iloc[train_indices]
 test_df = df.iloc[test_indices]
-
-class ConvNet(nn.Module):
-    def __init__(self, num_classes):
-        super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(32 * (64//4) * (64//4), 128)
-        self.fc2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 32 * (64//4) * (64//4))
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
 
 model = ConvNet(num_classes=26)
 
