f279c28b843cdca9e68754af79b32d776dca237f
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2024-03-23 15:36:41 +0000

    add get_id_from_mel_spectrogram and add main condition

--- RecordingDataset.py
+++ RecordingDataset.py
@@ -1,5 +1,8 @@
+import numpy as np
+import pandas as pd
 import torch
 from torch.utils.data import Dataset
+
 
 class RecordingDataset(Dataset):
     def __init__(self, df, data_path):
@@ -22,4 +25,32 @@ class RecordingDataset(Dataset):
     
     def get_label_from_target(self, target):
         return self.df[self.df['target'] == target].iloc[0, 2]
-        
+    
+    # RecordingDataset must be initialised with the dataframe that created the
+    # mel_spectrogram due to the randomness of data augmentation
+    def get_id_from_mel_spectrogram(self, mel_spectrogram):
+        for i in range(len(self.df)):
+            if np.array_equal(self.df['mel_spectrogram'].iloc[i], mel_spectrogram):
+                break
+        return self.df.iloc[i]['id']
+    
+
+if __name__ == "__main__":
+    mel_spec = np.load("mel_spectrogram.npy")
+    import training_data_processor as tdp
+    RECORDINGS_DIR = "Recordings"
+    WINDOW_SIZE = 1023
+    HOP_SIZE = 225
+    BEFORE = int(0.2 * 14400)
+    AFTER = int(0.8 * 14400)
+
+    file_paths = tdp.get_file_paths(RECORDINGS_DIR)
+    df_relative_paths, df_labels, df_targets, df_mel_spectrograms = tdp.process_recordings(file_paths, WINDOW_SIZE, HOP_SIZE, BEFORE, AFTER)
+    df = tdp.to_dataframe(df_relative_paths, df_labels, df_targets, df_mel_spectrograms)
+    dataset = RecordingDataset(df, "Recordings")
+    np.save("dataset.npy", dataset.df)
+
+    ds = np.load("dataset.npy", allow_pickle=True)
+    ds = pd.DataFrame(ds, columns=["id", "relative_path", "label", "target", "mel_spectrogram"])
+    id = RecordingDataset(ds, "Recordings").get_id_from_mel_spectrogram(mel_spec)
+    print(id)
