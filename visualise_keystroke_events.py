379ef43fbdde1e7de01eddbe2cf10a441b466e7c
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2024-05-06 14:31:43 +0100

    update keystroke visualiser with SpecAugment methods

--- visualise_keystroke_events.py
+++ visualise_keystroke_events.py
@@ -112,11 +112,68 @@ def plot_extracted_keystrokes(extracted_keystrokes, samplerate):
     fig.supylabel("Amplitude")
     plt.show()
 
+def display_mel_spectrograms(mel_spectrograms, samplerate, window_size, hop_size):
+    from math import floor, sqrt
+    import matplotlib.ticker as ticker
+    base = sqrt(len(mel_spectrograms))
+    rows = floor(base)
+    cols = rows
+    while rows * cols < len(mel_spectrograms):
+        cols += 1
+        if cols - rows > 1:
+            rows += 1
+            cols = rows
+    titles = ["Original", "Time-masked", "Frequency-masked", "Time and Frequency-masked"]
+    fig, axs = plt.subplots(rows, cols, figsize=(15, 15), constrained_layout=True)
+    for i, mel_spectrogram in enumerate(mel_spectrograms):
+        row = i // cols
+        col = i % cols
+        mel_show = librosa.display.specshow(librosa.power_to_db(mel_spectrogram, ref=np.max), y_axis="mel", x_axis="time", ax=axs[row, col], win_length=window_size, hop_length=hop_size, sr=samplerate, cmap="inferno")
+        axs[row, col].set_title(titles[i])
+        axs[row, col].set_xlabel("")
+        axs[row, col].set_ylabel("")
+    
+    for i in range(len(mel_spectrograms), rows * cols):
+        row = i // cols
+        col = i % cols
+        fig.delaxes(axs[row, col])
+
+    fig.colorbar(mel_show, ax=axs.ravel().tolist(), format="%+2.0f dB")
+    fig.suptitle("Mel Spectrograms")
+    fig.supxlabel("Time (s)")
+    fig.supylabel("Frequency (Hz)")
+    plt.show()
+
+
+def generate_mel_spectrogram(signal, samplerate, window_size, hop_size):
+    mel_spectrogram = librosa.feature.melspectrogram(y=signal, sr=samplerate, n_mels=64, n_fft=window_size, hop_length=hop_size)
+    return mel_spectrogram
+
+def mel_spectrogram_data_augmentation(mel_spectrograms):
+    import torchaudio.transforms as T
+    import torch
+    augmented_mel_spectrograms = []
+    for mel_spectrogram in mel_spectrograms:
+        time_mask = T.TimeMasking(time_mask_param=len(mel_spectrogram) / 10)
+        freq_mask = T.FrequencyMasking(freq_mask_param=len(mel_spectrogram) / 10)
+        augmented_mel_spectrograms.append(mel_spectrogram)
+
+        time_masked = time_mask(torch.tensor(mel_spectrogram))
+        augmented_mel_spectrograms.append(time_masked.numpy())
+
+        freq_masked = freq_mask(torch.tensor(mel_spectrogram))
+        augmented_mel_spectrograms.append(freq_masked.numpy())
+
+        # time_freq_masked = freq_mask(time_masked)
+        time_freq_masked = np.minimum(freq_masked, time_masked)
+        augmented_mel_spectrograms.append(time_freq_masked.numpy())
+    return augmented_mel_spectrograms
+
 if __name__ == "__main__":
     WINDOW_SIZE = 1023
     HOP_SIZE = 225
-    BEFORE = int(0.2 * 14400)
-    AFTER = int(0.8 * 14400)
+    BEFORE = int(0.3 * 14400)
+    AFTER = int(0.7 * 14400)
     RECORDING_DIR = "Recordings"
     FILE_PATH = os.path.join(RECORDING_DIR, "A.wav")
     signal, samplerate = load_recording(FILE_PATH)
@@ -156,3 +213,17 @@ if __name__ == "__main__":
     plt.colorbar(format="%+2.0f dB")
     plt.title("Keystroke 23")
     plt.show()
+
+    # signal and time-shifted signal
+    fig, axs = plt.subplots(2, 1, figsize=(10, 6), constrained_layout=True)
+    axs[0].plot(to_plot, label="Original")
+    axs[0].set_title("Original Keystroke")
+    axs[1].plot(np.roll(to_plot, (int(len(to_plot) * 0.1))), label="Time-shifted", color="r")
+    axs[1].set_title("Time-shifted Keystroke")
+    plt.show()
+
+    # signal and frequency-shifted signal
+    mel_spectrogram = generate_mel_spectrogram(to_plot, samplerate, WINDOW_SIZE, HOP_SIZE)
+    mel_spectrograms = [mel_spectrogram]
+    augmented_mel_spectrograms = mel_spectrogram_data_augmentation(mel_spectrograms)
+    display_mel_spectrograms(augmented_mel_spectrograms, samplerate, WINDOW_SIZE, HOP_SIZE)
